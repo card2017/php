ef7a3102f3698771e31926c199233665f86f7e63
card2017 <35178442+card2017@users.noreply.github.com>
2022-01-11 16:32:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
+//Variavel teste
+$nome = "Maria";
+$sobrenome = "C";
+var_dump($nome);
+echo "<br/>";
+	echo $nome . " " . $sobrenome;
+$nome1 = "Xeque";
+unset($nome1);
 
-echo "Ola 11012022b";
+if (isset($nome1)){
+	echo $nome1;
 
+}
 ?>
+
